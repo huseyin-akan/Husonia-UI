e7f47cff60f40307013c0311590f01832edd1506
husokanus <husokanus@gmail.com>
2023-07-25 02:23:11 +0300

    hus-todos file added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,3 @@ export default function Home() {
     </main>
   )
 }
-
-// Next.js Crash course: 31.26
-// Next-js : 20:59
-// REsponsive : 21:52
-// Fifa Card will be added to the right side of the hello screen
-
-
-//TODOS
-//1. Drag and drop cards with animation.
