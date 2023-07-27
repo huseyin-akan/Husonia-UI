38c7ba71ae461acda378119cadf2db05311cf502
huseyin-akan <semakarbav06@gmail.com>
2023-07-27 23:43:48 +0300

    mongoose setup

--- husokania-api/src/index.ts
+++ husokania-api/src/index.ts
@@ -1,8 +1,14 @@
 import express, {Request, Response} from 'express';
+import mongoose from 'mongoose';
+
 const app = express();
+const db = mongoose.connect('mongodb+srv://husoka:AQjn1gUwfXNAtiN9@atlascluster.bhxjspv.mongodb.net/Husonia?retryWrites=true&w=majorit');
+console.log('Connected to DB...')
 
 app.get('/', (req : Request, res: Response) => {
     res.send('Hello Mazafaka with nodemon 2')
 })
 
-app.listen(5069);
+app.listen(5069, () => {
+    console.log('App running on port number : 5069')
+});
